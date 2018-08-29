13719a2ff44c23920dac0cde04ba94b13f5e34fb
achinnasamy <chinnasamyad@gmail.com>
2018-08-29 09:06:35 +0530

    Transformations

--- src/AllTransformations.java
+++ src/AllTransformations.java
@@ -5,6 +5,8 @@ import org.apache.spark.sql.SparkSession;
 import org.apache.spark.storage.StorageLevel;
 import scala.Tuple2;
 
+import java.util.Arrays;
+
 public class AllTransformations {
 
     public static void main(String args[]) {
@@ -12,7 +14,7 @@ public class AllTransformations {
 
         SparkSession spark = SparkSession
                 .builder()
-                .appName("CustomPartitionerJOB")
+                .appName("TransformationsJOB")
                 .master("local[*]")
                 .getOrCreate();
 
@@ -21,9 +23,9 @@ public class AllTransformations {
 
         JavaRDD<String> textFileRDD = javaSparkContext.textFile("/Users/dharshekthvel/ac/code/scalatrainingintellij/data/auth.csv");
 
-        JavaPairRDD<String, Integer> pairRDD = textFileRDD.mapToPair(each -> new Tuple2<String,Integer>(each.split(",")[3],1));
+        //JavaPairRDD<String, Integer> pairRDD = textFileRDD.mapToPair(each -> new Tuple2<String,Integer>(each.split(",")[3],1));
 
-        pairRDD.reduceByKey((x,y) -> x+y).foreach(each -> System.out.println(each._1 + " --- " + each._2));
+        //pairRDD.reduceByKey((x,y) -> x+y).foreach(each -> System.out.println(each._1 + " --- " + each._2));
 
 
         textFileRDD.union(textFileRDD);
@@ -32,8 +34,33 @@ public class AllTransformations {
         textFileRDD.pipe("ls -l");
 
 
-        textFileRDD.checkpoint();
-        textFileRDD.persist(StorageLevel.MEMORY_AND_DISK());
+        //textFileRDD.checkpoint();
+        //textFileRDD.persist(StorageLevel.MEMORY_AND_DISK());
+
+
+
+        /* Cartesian Transformation */
+        //JavaRDD<String> rddStrings = javaSparkContext.parallelize(Arrays.asList("A","B","C"));
+        //JavaRDD<Integer> rddIntegers = javaSparkContext.parallelize(Arrays.asList(77,88,99));
+
+        //rddStrings.cartesian(rddIntegers).foreach(each -> System.out.println(each._1 + " --- " + each._2));;
+
+
+
+
+        JavaPairRDD<String, Integer> pairRDD =
+                    javaSparkContext.parallelizePairs(
+                            Arrays.asList(new Tuple2<String, Integer>("B", 2),
+                                            new Tuple2<String, Integer>("C", 5),
+                                            new Tuple2<String, Integer>("D", 7),
+                                            new Tuple2<String, Integer>("A", 8)));
+
+        // Count By Key is an action on PairRDD
+        pairRDD.countByKey();
+
+
+
+
 
     }
 }
